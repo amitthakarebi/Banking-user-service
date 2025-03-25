a5eb674d6203ea6afa9a4262bbb315a14dd56562
amitthakarebi <amit.thakarebi@gmail.com>
2025-03-26 01:50:17 +0530

    Update UserService.java
    Added extra validation while updating the user.

--- src/main/java/com/banking/user_service/services/UserService.java
+++ src/main/java/com/banking/user_service/services/UserService.java
@@ -45,17 +45,17 @@ public class UserService {
         }
         LocalDateTime localDateTime = LocalDateTime.now();
         // First name
-        if (user.getFirstName() != null) {
+        if (user.getFirstName() != null && !user.getFirstName().isEmpty()) {
             existingUser.setFirstName(user.getFirstName());
         }
 
         // Middle name
-        if (user.getMiddleName() != null) {
+        if (user.getMiddleName() != null && !user.getMiddleName().isEmpty()) {
             existingUser.setMiddleName(user.getMiddleName());
         }
 
         // Last name
-        if (user.getLastName() != null) {
+        if (user.getLastName() != null && !user.getLastName().isEmpty()) {
             existingUser.setLastName(user.getLastName());
         }
 
@@ -65,17 +65,17 @@ public class UserService {
         }
 
         // Email
-        if (user.getEmail() != null) {
+        if (user.getEmail() != null && !user.getEmail().isEmpty()) {
             existingUser.setEmail(user.getEmail());
         }
 
         // Mobile
-        if (user.getMobile() != null) {
+        if (user.getMobile() != null && user.getMobile().isEmpty()) {
             existingUser.setMobile(user.getMobile());
         }
 
         // Address
-        if (user.getAddress() != null) {
+        if (user.getAddress() != null && user.getAddress().isEmpty()) {
             existingUser.setAddress(user.getAddress());
         }
 
